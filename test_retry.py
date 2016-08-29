4e9c52bcfc8cbb898c79bab5b19da73d41b78729
Michel Alexandre Salim <michel@traveloka.com>
2016-08-29 14:57:55 +0700

    Add example of retry not working if exception is caught

--- test_retry.py
+++ test_retry.py
@@ -14,6 +14,7 @@ RATE_LIMIT_ERROR = ClientError(
     RATE_LIMIT_RESPONSE,
     'DescribeInstances')
 
+
 class AWSClass(object):
 
     def list_instances(self):
@@ -27,6 +28,21 @@ class AWSClass(object):
     def list_instances_with_3_attempts(self):
         return self.list_instances()
 
+    @retry(stop_max_attempt_number=3)
+    def list_instances_catch_exceptions_with_3_attempts(self):
+        try:
+            return self.list_instances()
+        except Exception as e:
+            return None
+
+
+def test_expect_no_exception():
+    aws_instance = AWSClass()
+    aws_instance.list_instances = MagicMock(
+        side_effect=RATE_LIMIT_ERROR)
+    aws_instance.list_instances_catch_exceptions_with_3_attempts()
+    assert aws_instance.list_instances.call_count == 1
+
 
 def test_expect_exception():
     aws_instance = AWSClass()
@@ -34,6 +50,7 @@ def test_expect_exception():
         side_effect=RATE_LIMIT_ERROR)
     with pytest.raises(ClientError):
         aws_instance.list_instances()
+
 
 
 def test_expect_2_attempts():
